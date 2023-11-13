5c7c2138fefb2759d3c8ff46df194d1416ba1850
Flowy1337 <tobias.sieber@stud.uni-heidelberg.de>
2023-11-13 21:50:25 +0100

    UI

---
+++ Assets/UI_Scripts/Cursor.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+public class Cursorbehav : MonoBehaviour
+{
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.tag != "UI")
+        {
+            // Der Mauszeiger ist über dem Hintergrundbild
+            Cursor.visible = true;
+        }
+
+        // Get the current mouse position in screen space
+        Vector2 mousePosition = Input.mousePosition;
+
+        // Convert the screen position to world space
+        mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, -Camera.main.transform.position.z));
+
+        // Set the GameObject position to the mouse position
+        transform.position = mousePosition;
+    }
+}
---
+++ Assets/UI_Scripts/Statbar.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statbar : MonoBehaviour
+{
+    private void OnMouseExit()
+    {
+        Cursor.visible = true;
+    }
+}
