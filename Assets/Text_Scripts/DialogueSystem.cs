a7bd60076ed2301544a76826580a3105001d5776
Flowy1337 <tobias.sieber@stud.uni-heidelberg.de>
2023-11-17 15:17:45 +0100

    Text_Script added

---
+++ Assets/Text_Scripts/DialogueSystem.cs
@@ -0,0 +1,57 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    [SerializeField] private DialogueContainer dialogueContainer= new DialogueContainer();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueSystem  : MonoBehaviour
+{
+    public DialogueContainer dialogueContainer = new DialogueContainer();
+
+    public static DialogueSystem instance;
+
+    private void Awake(){
+        if(instance==null){
+            instance=this;
+
+        }
+        else{
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Your start logic here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Your update logic here
+    }
+}
